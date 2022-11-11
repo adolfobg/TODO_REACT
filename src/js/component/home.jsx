7c5daede2a550d10670e5560749193e93499cf6c
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-11-11 17:24:38 +0000

    Se añade navbar a la web

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import propTypes from "prop-types";
+import add from "./notes.jsx";
+
+
 
 
 //include images into your bundle
@@ -6,16 +10,46 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState()
-	const [inputData, setInputData] = useState()
+	const [inputNotes, setInputNotes] = useState();
+	const [inputNew, setInputNew] = useState();
+	const data_entry = (event) => {
+		setInputNotes(event.target.value)
+	};
+	const key_down = (e) => {
+		if (e.keyCode == 13) {
+			return (
+				<>
+					setInputNew(inputNotes);
+					<div className="input_value">{inputNew}</div>
+				</>
+			);
+		}
+	};
 
 	return (
 		<>
-		<input type="text" className="input" />
+			<div className="container">
+				<div className="row">
+					<input type="text" className="input col-5"
+						onChange={data_entry}
+						onKeyDown={key_down}
+					/>
 
-		<div className="input_value"></div>
+				</div>
+			</div>
 		</>
 	);
 };
 
 export default Home;
+
+
+
+
+
+/*const AnyComponent = () => {
+		const [inputValue, setInputValue ] = useState('');
+		return (
+		<input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} />
+	);
+};*/
---
+++ src/js/component/notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+const add = () => {
+    return (
+        <>
+            <div className="input_value">{inputNew}</div>
+        </>
+    );
+};
+
+export default add;
